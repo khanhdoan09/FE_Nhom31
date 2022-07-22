c18ff7967415ca7836e147929eb23a53a978a821
Phan Văn Hùng <91454052+hungphan0501@users.noreply.github.com>
2022-07-22 21:48:27 +0700

    code giao diện register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./view/home/home.component";
 import {LoginComponent} from "./view/authentication/login/login.component";
+import {RegisterComponent} from "./view/authentication/register/register.component";
 
 
 const routes: Routes = [
   {
     path: "logIn",
     component: LoginComponent,
+  },
+  {
+    path: "register",
+    component: RegisterComponent,
   },
   {
     path: "home",

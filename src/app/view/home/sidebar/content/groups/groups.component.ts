8645c31bbc773f5170f56b8f5dc8817f036ab9aa
minhchanh19130021 <93698433+minhchanh19130021@users.noreply.github.com>
2022-07-24 10:35:53 +0700

    Tải danh sách tin nhắn nhóm

--- src/app/view/home/sidebar/content/groups/groups.component.ts
+++ src/app/view/home/sidebar/content/groups/groups.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MdbModalRef} from "mdb-angular-ui-kit/modal";
+import {TestConnectService} from 'src/app/service/api/testConnectService';
+import {Api} from "../../../../../service/api/api";
 
 @Component({
   selector: 'app-groups',
@@ -8,7 +10,42 @@ import {MdbModalRef} from "mdb-angular-ui-kit/modal";
 })
 export class GroupsComponent implements OnInit {
   isShowing = false;
-  constructor() { }
+  groupList : Array<any> = [];
+  groupListAvavatar : Array<any> = [];
+  avatarG:any;
+  constructor(
+    private _testConnectService: TestConnectService
+  ) {
+    this.updateGroupList();
+  }
+
+  init() {
+    setTimeout(() => {
+      this._testConnectService.messages.subscribe(msg => {
+        this.renderGroupList(msg);
+      });
+      setTimeout(() => {
+        this._testConnectService.messages.next(Api.loadUserList());
+      }, 500)
+    }, 500)
+  }
+
+  updateGroupList() {
+    setTimeout(() => {
+      this.init();
+    }, 1500)
+  }
+
+   renderGroupList(msg: any) {
+     console.log('Danh sách tin nhắn nhóm')
+     for (let u of msg.data) {
+       if (u.type === 1) {
+         this.groupList.push(u);
+       }
+     }
+     console.log(this.groupList);
+     return this.groupList;
+   }
 
   ngOnInit(): void {
   }

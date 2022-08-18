29925dcee028209f7fcf351effb754f0eb405dbb
minhchanh19130021 <93698433+minhchanh19130021@users.noreply.github.com>
2022-08-18 15:54:30 +0700

    Tải ảnh đại diện cho danh sách tin nhắn

--- src/app/service/home/sidebar/chats-sidebar/chats-sidebar.service.ts
+++ src/app/service/home/sidebar/chats-sidebar/chats-sidebar.service.ts
@@ -4,6 +4,7 @@ import {Api} from "../../../api/api";
 import {Contact, ContactTo} from "../../../../model/contact-to";
 import {resetPagination} from "../../../../model/pagination";
 import {set} from "@angular/fire/database";
+import {AngularFireStorage} from "@angular/fire/compat/storage";
 
 @Injectable({
   providedIn: 'root'
@@ -15,38 +16,38 @@ export class ChatsSidebarService {
   test: any = true;
   time: string = "";
 
-  constructor(private _testConnectService: TestConnectService) {
+  constructor(private _testConnectService: TestConnectService, private afStorage: AngularFireStorage) {
   }
 
   runService() {
+
     this.updateUser();
+
   }
 
   init = () => {
     // wait for login
-    setTimeout(() => {
-      // first invoke observable by subscribe function
-      this._testConnectService.messages.subscribe(msg => {
-        this.renderListUser(msg);
-        this.checkStatusUser(msg)
-      });
-      setTimeout(() => {
-        // second send signal next then observable will catch it
-        this._testConnectService.messages.next(Api.loadUserList());
-      }, 300)
 
-    }, 300)
+    // first invoke observable by subscribe function
+    this._testConnectService.messages.subscribe(msg => {
+      this.renderListUser(msg);
+      this.checkStatusUser(msg)
+      this.getAvatarUser(msg);
+    });
+
+    // second send signal next then observable will catch it
+    this._testConnectService.messages.next(Api.loadUserList());
+
+
   }
 
   updateUser() {
-    // success  edit 'setTimeout' => 'setInterval'
-    setTimeout(() => {
-      this.init();
-    }, 1000)
+    this.init();
   }
 
   renderListUser(msg: any) {
     this.userList = msg.data;
+
     return this.userList;
   }
 
@@ -77,6 +78,42 @@ export class ChatsSidebarService {
       return
     }
   }
+
+  getAvatarUser(msg: any) {
+    setInterval(() => {
+      this.getOneAvatar(msg.data, 0)
+    }, 2500)
+  }
+
+  getOneAvatar(arrUser: any, index: number) {
+    try {
+      if (index != arrUser.length) {
+        if (arrUser[index].type === 0) {
+          let storageRef = this.afStorage.storage.ref().child("avatar/" + arrUser[index].name);
+          storageRef.getDownloadURL().then(urlFB => {
+              this.getOneAvatar(arrUser, index + 1)
+              arrUser[index].src = urlFB
+
+            },
+            // () => {
+            //   arrUser[index].src = 'https://www.w3schools.com/howto/img_avatar.png';
+            //   this.getOneAvatar(arrUser, index + 1)
+            // }
+          ).catch((error) => {
+            arrUser[index].src = 'https://www.w3schools.com/howto/img_avatar.png';
+            this.getOneAvatar(arrUser, index + 1);
+          })
+
+        }
+      } else {
+
+        return;
+      }
+    } catch (e) {
+
+    }
+  }
+
 
   selectMessage(contact: Contact) {
     resetPagination()

ee1a68df9773745eec73525ab5159bc17b404cba
Đoàn Trần Phi Khánh <80077540+khanhdoan09@users.noreply.github.com>
2022-08-22 10:56:08 +0700

    last

--- src/app/view/home/chat/header-bar/header-bar.component.ts
+++ src/app/view/home/chat/header-bar/header-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Contact, ContactTo,  IdSetInterval} from "../../../../model/contact-to";
+import {ArrayAvatar, Contact, ContactTo, CurrentUser, IdSetInterval} from "../../../../model/contact-to";
 import {AngularFireStorage} from "@angular/fire/compat/storage";
 import {Api} from "../../../../service/api/api";
 import {ConnectApi} from "../../../../service/websocket/connect-api";
@@ -15,12 +15,13 @@ export class HeaderBarComponent implements OnInit {
   avatar!:string;
 
   constructor(private afStorage: AngularFireStorage, private connect: ConnectApi) {
-      ContactTo.contactTo.subscribe((contact:Contact)=>{
-        this.userName = contact.name;
-        IdSetInterval.idSetIntervalContactTo = setInterval(()=>{
-          this.checkStatus();
-        }, 1500)
-      })
+    ContactTo.contactTo.subscribe((contact:Contact)=>{
+      this.userName = contact.name;
+      IdSetInterval.idSetIntervalContactTo = setInterval(()=>{
+        this.getUrlImageFromFirebase(this.userName);
+        this.checkStatus();
+      }, 1500)
+    })
   }
 
   checkStatus() {
@@ -30,7 +31,14 @@ export class HeaderBarComponent implements OnInit {
     this.connect.subject?.next(Api.checkStatus(this.userName));
   }
 
-
+  getUrlImageFromFirebase(contactToUsername: string) {
+    let storageRef = this.afStorage.storage.ref().child("avatar/" + contactToUsername);
+    return storageRef.getDownloadURL().then(urlFB => {
+      this.avatar = urlFB;
+    }, ()=>{
+      this.avatar = 'https://www.w3schools.com/howto/howto_css_image_avatar.asp';
+    });
+  }
 
   ngOnInit(): void {
   }
